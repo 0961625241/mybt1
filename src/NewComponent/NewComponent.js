cff25bc7427de5fb7762544c5de5f7a5a19d3092
qua <ddrduongqua1027@gmail.com>
2019-05-12 09:25:54 +0700

    add new project

---
+++ src/NewComponent/NewComponent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './NewComponent.css'
+class NewComponent extends Component {
+    render() {
+        
+        return (
+            <div>
+                <h1 id="custom-h1" >hello wa</h1>
+            </div>
+        );
+    }
+}
+
+export default NewComponent;
